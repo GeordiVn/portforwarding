568bfe5a360d32316e59188bdba16c5962f53aca
GeordiVn <unconfigured@null.spigotmc.org>
2023-09-21 08:22:12 +0200

    gui added

---
+++ PortForwarder/Forward.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace PortMapper
+{
+    internal class Forward
+    {
+
+        private readonly Socket _mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private TextBox textBoxInfo;
+        private Application application;
+        public Forward(TextBox textBox , Application application) 
+        {
+            this.textBoxInfo = textBox;
+            this.application = application;
+        }
+        public void Start(IPEndPoint local, IPEndPoint remote)
+        {
+            _mainSocket.Bind(local);
+            _mainSocket.Listen(10);
+
+            while (true)
+            {
+                var source = _mainSocket.Accept();
+
+                application.Dispatcher.Invoke(new Action(() => { textBoxInfo.AppendText("Connection Accepted: ip"+ local.Address.ToString()+ " Port: "+local.Port.ToString()+" To Remote"+ remote.Address.ToString()+" Port: "+remote.Port.ToString() +"\n"); }));
+                var destination = new Forward(textBoxInfo,application);
+                var state = new State(source, destination._mainSocket);
+                destination.Connect(remote, source);
+                source.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
+            }
+        }
+
+        private void Connect(EndPoint remoteEndpoint, Socket destination)
+        {
+            var state = new State(_mainSocket, destination);
+            _mainSocket.Connect(remoteEndpoint);
+            _mainSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, OnDataReceive, state);
+        }
+
+        private static void OnDataReceive(IAsyncResult result)
+        {
+            var state = (State)result.AsyncState;
+            try
+            {
+                var bytesRead = state.SourceSocket.EndReceive(result);
+                if (bytesRead > 0)
+                {
+                    state.DestinationSocket.Send(state.Buffer, bytesRead, SocketFlags.None);
+                    state.SourceSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
+                }
+            }
+            catch
+            {
+                state.DestinationSocket.Close();
+                state.SourceSocket.Close();
+            }
+        }
+
+        private class State
+        {
+            public Socket SourceSocket { get; private set; }
+            public Socket DestinationSocket { get; private set; }
+            public byte[] Buffer { get; private set; }
+
+            public State(Socket source, Socket destination)
+            {
+                SourceSocket = source;
+                DestinationSocket = destination;
+                Buffer = new byte[8192];
+            }
+        }
+    }
+
+}
+
---
+++ PortForwarder/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using PortMapper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+namespace PortForwarder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            readIpPortListAndPutInTextBox();
+
+            //bijvoorbeeld 127.0.0.1:8080 & 192.168.0.110:9090;
+
+            string[] ipPortListLineSplit = textBoxListIpRemote.Text.Split(';');
+
+
+            foreach (string line in ipPortListLineSplit) 
+            {
+                if(line.Contains(":")) 
+                {
+                    string[] ipPortSplit = line.Split('&');
+                    string[] ipPortLocal = ipPortSplit[0].Split(":");
+                    string[] ipPortRemote = ipPortSplit[1].Split(":");
+                    try {
+
+                        Thread thread = new Thread(new ThreadStart(() => {
+
+                            try {
+                                Forward forward = new Forward(textBoxInfo,Application.Current);
+
+                                forward.Start(new System.Net.IPEndPoint(IPAddress.Parse(ipPortLocal[0].Trim()), Int32.Parse(ipPortLocal[1].Trim())),
+                                              new System.Net.IPEndPoint(IPAddress.Parse(ipPortRemote[0].Trim()), Int32.Parse(ipPortRemote[1].Trim())));
+                            }
+                            catch (Exception ex) { Application.Current.Dispatcher.Invoke(new Action(() => { textBoxInfo.AppendText(ex.Message + "\n"+"Ip local: " + ipPortLocal[0]+" Port local: "+ ipPortLocal[1] + " Ip remote: "+ ipPortRemote[0] + " Port remote: "+ ipPortRemote[1]); })); }
+
+                            
+
+                        }));
+                        thread.Start();
+                    }
+                    catch(Exception ex) { textBoxInfo.AppendText(ex.Message + "\n"); }
+
+                    
+                }
+            }
+
+        }
+
+        public void readIpPortListAndPutInTextBox() 
+        {
+            string[] ipPortListFile = File.ReadAllLines(Environment.CurrentDirectory+"/IpPortList.txt");
+
+            foreach (string ipPort in ipPortListFile) 
+            {
+                if (ipPort.Length > 5) 
+                {
+                    textBoxListIpRemote.AppendText(ipPort+"\n");
+                }
+            }
+        }
+    }
+}
