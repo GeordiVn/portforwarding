6327f5eaf19b9f7f223d7948ce3229504ab2d749
GeordiVn <unconfigured@null.spigotmc.org>
2023-09-21 20:23:12 +0200

    layout verbeterd

--- PortForwarder/MainWindow.xaml.cs
+++ PortForwarder/MainWindow.xaml.cs
@@ -34,40 +34,67 @@ namespace PortForwarder
 
             //bijvoorbeeld 127.0.0.1:8080 & 192.168.0.110:9090;
 
+            buttonStart.Click += ButtonStart_Click;
+            //(s, e) => 
+            // {
+            // startListening();
+            // };
+            buttonAddRule.Click += ButtonAddRule_Click;
+
+        }
+
+        private void ButtonAddRule_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("clicked");
+            textBoxInfo.Text += "clicked!";
+            if (addLocalIpTextBox.Text.Length > 0 && addLocalPortTextBox.Text.Length > 0 && addRemoteIpTextBox.Text.Length > 0 && addRemotePortTextBox.Text.Length > 0)
+            {
+                string lineToAddToFile = addLocalIpTextBox.Text + ":" + addLocalPortTextBox.Text + "&" + addRemoteIpTextBox.Text + ":" + addRemotePortTextBox.Text + ";";
+
+                File.AppendAllText(Environment.CurrentDirectory + "/IpPortList.txt", "\n" + lineToAddToFile);
+                textBoxInfo.Text += "added ??";
+            }
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
             string[] ipPortListLineSplit = textBoxListIpRemote.Text.Split(';');
 
 
-            foreach (string line in ipPortListLineSplit) 
+            foreach (string line in ipPortListLineSplit)
             {
-                if(line.Contains(":")) 
+                if (line.Contains(":"))
                 {
                     string[] ipPortSplit = line.Split('&');
                     string[] ipPortLocal = ipPortSplit[0].Split(":");
                     string[] ipPortRemote = ipPortSplit[1].Split(":");
-                    try {
+                    try
+                    {
 
                         Thread thread = new Thread(new ThreadStart(() => {
 
-                            try {
-                                Forward forward = new Forward(textBoxInfo,Application.Current);
+                            try
+                            {
+                                Forward forward = new Forward(textBoxInfo, Application.Current);
 
                                 forward.Start(new System.Net.IPEndPoint(IPAddress.Parse(ipPortLocal[0].Trim()), Int32.Parse(ipPortLocal[1].Trim())),
                                               new System.Net.IPEndPoint(IPAddress.Parse(ipPortRemote[0].Trim()), Int32.Parse(ipPortRemote[1].Trim())));
                             }
-                            catch (Exception ex) { Application.Current.Dispatcher.Invoke(new Action(() => { textBoxInfo.AppendText(ex.Message + "\n"+"Ip local: " + ipPortLocal[0]+" Port local: "+ ipPortLocal[1] + " Ip remote: "+ ipPortRemote[0] + " Port remote: "+ ipPortRemote[1]); })); }
+                            catch (Exception ex) { Application.Current.Dispatcher.Invoke(new Action(() => { textBoxInfo.AppendText(ex.Message + "\n" + "Ip local: " + ipPortLocal[0] + " Port local: " + ipPortLocal[1] + " Ip remote: " + ipPortRemote[0] + " Port remote: " + ipPortRemote[1]); })); }
 
-                            
+
 
                         }));
                         thread.Start();
                     }
-                    catch(Exception ex) { textBoxInfo.AppendText(ex.Message + "\n"); }
+                    catch (Exception ex) { textBoxInfo.AppendText(ex.Message + "\n"); }
 
-                    
+
                 }
             }
-
         }
+
+      
 
         public void readIpPortListAndPutInTextBox() 
         {
